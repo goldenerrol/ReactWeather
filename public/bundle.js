3cbefbc29c7e9dd7ad34294d75d309502265357d
Dan Goldberg <d.goldberg82@gmail.com>
2016-08-31 10:10:47 -0400

    Style and Copy in About page

--- public/bundle.js
+++ public/bundle.js
@@ -26561,7 +26561,7 @@
 /* 251 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 	
 	var React = __webpack_require__(8);
 	
@@ -26575,17 +26575,46 @@
 	
 	var About = function About(props) {
 	  return React.createElement(
-	    'div',
+	    "div",
 	    null,
 	    React.createElement(
-	      'h3',
-	      null,
-	      'About'
+	      "h1",
+	      { className: "text-center" },
+	      "About"
 	    ),
 	    React.createElement(
-	      'p',
+	      "p",
 	      null,
-	      'Welcome to the about page!'
+	      "This is a weather application built on React. I have built this for The Complete React App Developer Course."
+	    ),
+	    React.createElement(
+	      "p",
+	      null,
+	      "Here are some of the toold I used:"
+	    ),
+	    React.createElement(
+	      "ul",
+	      null,
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "https://facebook.github.io/react" },
+	          "React"
+	        ),
+	        " - This was the JavaScript framework used."
+	      ),
+	      React.createElement(
+	        "li",
+	        null,
+	        React.createElement(
+	          "a",
+	          { href: "http://openweathermap.org" },
+	          "Open Weather Map"
+	        ),
+	        " - Open Weather Map was used to search for weather data by city name."
+	      )
 	    )
 	  );
 	};
